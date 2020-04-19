adcdcc3e770e7c8e2c9c7f72b72798a1d2f5ac1d
Sebastian Morr <sebastian@morr.cc>
2020-04-19 14:04:52 +0200

    Golden region spawns cherries

--- main.gd
+++ main.gd
@@ -17,6 +17,7 @@ var angular_velocity = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
     $Music.play()
+    $Music.pitch_scale = pitch
     spectrum = AudioServer.get_bus_effect_instance(0,0)
 
 func _input(event):
@@ -31,17 +32,25 @@ func _process(delta):
     
     #var new_pitch = prev_m.distance_to(m)/50
     
-    var rotational_inertia = 100
-    var angular_acceleration = $RecordPlayer/Record/Player.torque/rotational_inertia
-    angular_velocity += angular_acceleration
-    angular_velocity -= 0.2*delta*angular_velocity
+    if $RecordPlayer/Player.global_position.distance_to($RecordPlayer/Record.global_position) < 500:
+        var rotational_inertia = 100
+        var angular_acceleration = $RecordPlayer/Player.torque/rotational_inertia
+        angular_velocity += angular_acceleration
+        var to_hamster = $RecordPlayer/Player.global_position-$RecordPlayer/Record.global_position
+        var forward = to_hamster.rotated(PI/2).normalized()
+        $RecordPlayer/Player.move_and_slide(forward*angular_velocity*to_hamster.length())
+    angular_velocity -= 0.05*delta*angular_velocity
     
-    var pitch = angular_velocity
+    pitch = angular_velocity
     #pitch = lerp(pitch, new_pitch, 0.005)
     
     #var f = spectrum.get_magnitude_for_frequency_range(0,100,1)
     #var db = linear2db(f.length())*10+600
     $RecordPlayer/Speed/Slider.position.x = $RecordPlayer/Speed/Line.points[0].x + max(0,pitch*0.5*($RecordPlayer/Speed/Line.points[1].x-$RecordPlayer/Speed/Line.points[0].x))
+    if golden():
+        $RecordPlayer/Speed/Slider.modulate = Color("b9b551")
+    else:
+        $RecordPlayer/Speed/Slider.modulate = Color("ffffff")  
     
     #var s = max(1,db/50)
     #var s2 = lerp($Re.scale.x, s, 0.05)
@@ -54,14 +63,18 @@ func _process(delta):
     $Music.pitch_scale = pitch
     
     $RecordPlayer/Record.rotation += delta*angular_velocity
-    $RecordPlayer/Record/Player.rotation -= delta*angular_velocity
+    #$RecordPlayer/Player.rotation -= delta*angular_velocity
     
     var progress = $Music.get_playback_position()/$Music.get_stream().get_length()
     $RecordPlayer/Arm.rotation_degrees = arm_rotation_start + (arm_rotation_end-arm_rotation_start)*progress
 
     prev_m = m
 
-#    if randi() % 200 == 0:
-#        var c = preload("res://cherry.tscn").instance()
-#        c.position = Vector2(rand_range(-400, 400), rand_range(-400, 400))
-#        $RecordPlayer/Record.add_child(c)
+
+    if randi() % 200 == 0 and golden() and len(get_tree().get_nodes_in_group("cherries")) < 3:
+        var c = preload("res://cherry.tscn").instance()
+        c.position = Vector2(rand_range(-400, 400), rand_range(-400, 400))
+        $RecordPlayer/Record.add_child(c)
+
+func golden():
+    return pitch > 0.9 and pitch < 1.1
--- player.gd
+++ player.gd
@@ -17,24 +17,23 @@ func _process(delta):
         dir = global_position.angle_to_point(get_global_mouse_position()) - PI/2# + get_parent().rotation
         down = -1
         speed = 400*min(50, global_position.distance_to(get_global_mouse_position()))/50
-        print(down)
-    
+        
     var movement = Vector2(right, down).normalized().rotated(dir)*speed
     
-    var to_center = (get_parent().global_position - global_position)
+    var to_center = (get_parent().find_node("Record").global_position - global_position)
     
-    var mass = 10000
-    var centrifugal_force = mass*pow(get_parent().get_parent().get_parent().angular_velocity, 2)/to_center.length()
-    #print(centrifugal_force)
-    movement -= centrifugal_force*to_center.normalized()
+#    if to_center.length() < 500:
+#        var mass = 10000
+#        var centrifugal_force = mass*pow(get_parent().get_parent().angular_velocity, 2)/to_center.length()
+#        #print(centrifugal_force)
+#        movement -= centrifugal_force*to_center.normalized()
     
     move_and_slide(movement)
     
-    var forward = to_center.rotated(PI/2).normalized()
-    var projected = movement.dot(forward)
+    #var forward = to_center.rotated(PI/2).normalized()
+    #var projected = movement.dot(forward)
     #torque = projected/500#/500*(500-position.length())/150.0
-    torque = -Vector3(to_center.x, 0, to_center.y).cross(Vector3(movement.x, 0, movement.y)).y/300000
-    #print(torque)
-
+    torque = -Vector3(to_center.x, 0, to_center.y).cross(Vector3(movement.x, 0, movement.y)).y/1000000
+   
     if Vector2(right,down) != Vector2.ZERO and speed > 10:
         $Sprite.rotation = 0.1*sin(OS.get_system_time_msecs()/50)
