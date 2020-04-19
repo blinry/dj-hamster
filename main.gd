0ba9b80053673da9e337abc8e94f351fe414d75b
Sebastian Morr <sebastian@morr.cc>
2020-04-19 13:01:55 +0200

    Better physics

--- main.gd
+++ main.gd
@@ -6,16 +6,17 @@ extends Node2D
 # var b = "text"
 
 var spectrum
-var pitch = 0
+var pitch = 0.000001
 var prev_m = Vector2(0,0)
 
 var arm_rotation_start = 0
 var arm_rotation_end = 22
 
+var angular_velocity = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $AudioStreamPlayer2D.play()
-    #$AudioStreamPlayer2D2.play()
+    $Music.play()
     spectrum = AudioServer.get_bus_effect_instance(0,0)
 
 func _input(event):
@@ -29,26 +30,38 @@ func _process(delta):
     var m = get_global_mouse_position()
     
     #var new_pitch = prev_m.distance_to(m)/50
-    var new_pitch = $RecordPlayer/Record/Player.force
-    pitch = lerp(pitch, new_pitch, 0.005)
     
-    var f = spectrum.get_magnitude_for_frequency_range(0,100,1)
-    var db = linear2db(f.length())*10+600
-    $RecordPlayer/Speed/Slider.position.x = $RecordPlayer/Speed/Line.points[0].x + pitch*0.5*($RecordPlayer/Speed/Line.points[1].x-$RecordPlayer/Speed/Line.points[0].x)
+    var rotational_inertia = 100
+    var angular_acceleration = $RecordPlayer/Record/Player.torque/rotational_inertia
+    angular_velocity += angular_acceleration
+    angular_velocity -= 0.2*delta*angular_velocity
     
-    var s = max(1,db/50)
+    var pitch = angular_velocity
+    #pitch = lerp(pitch, new_pitch, 0.005)
+    
+    #var f = spectrum.get_magnitude_for_frequency_range(0,100,1)
+    #var db = linear2db(f.length())*10+600
+    $RecordPlayer/Speed/Slider.position.x = $RecordPlayer/Speed/Line.points[0].x + max(0,pitch*0.5*($RecordPlayer/Speed/Line.points[1].x-$RecordPlayer/Speed/Line.points[0].x))
+    
+    #var s = max(1,db/50)
     #var s2 = lerp($Re.scale.x, s, 0.05)
     #print($Vis.scale.x)
     #$Vis.scale = Vector2(s2, s2)
     #$Player.speed = pitch*100
 
     #pitch *= 0.999
-    $AudioStreamPlayer2D.pitch_scale = pitch
+    #print(pitch)
+    $Music.pitch_scale = pitch
     
-    $RecordPlayer/Record.rotation += delta*pitch
-    $RecordPlayer/Record/Player.rotation -= delta*pitch
+    $RecordPlayer/Record.rotation += delta*angular_velocity
+    $RecordPlayer/Record/Player.rotation -= delta*angular_velocity
     
-    var progress = $AudioStreamPlayer2D.get_playback_position()/$AudioStreamPlayer2D.get_stream().get_length()
+    var progress = $Music.get_playback_position()/$Music.get_stream().get_length()
     $RecordPlayer/Arm.rotation_degrees = arm_rotation_start + (arm_rotation_end-arm_rotation_start)*progress
 
     prev_m = m
+
+#    if randi() % 200 == 0:
+#        var c = preload("res://cherry.tscn").instance()
+#        c.position = Vector2(rand_range(-400, 400), rand_range(-400, 400))
+#        $RecordPlayer/Record.add_child(c)
--- player.gd
+++ player.gd
@@ -2,14 +2,32 @@ extends KinematicBody2D
 
 export var speed = 400
 var dir = 0
-var force = 0
+var torque = 0
 
 func _ready():
     pass
 
 func _process(delta):
-    dir = atan2(position.y, position.x) + get_parent().rotation
+    #dir = atan2(position.y, position.x) + get_parent().rotation
+    dir = 0
     var right = Input.get_action_strength("right") - Input.get_action_strength("left")
     var down = Input.get_action_strength("down") - Input.get_action_strength("up")
-    move_and_slide(Vector2(right, down).normalized().rotated(dir)*speed)
-    force = -down*(500-position.length())/150.0
+
+    if Input.is_action_pressed("guide"):
+        dir = global_position.angle_to_point(get_global_mouse_position()) - PI/2# + get_parent().rotation
+        down = -1
+        speed = 400*min(50, global_position.distance_to(get_global_mouse_position()))/50
+        print(down)
+    
+    var movement = Vector2(right, down).normalized().rotated(dir)*speed
+    move_and_slide(movement)
+    
+    var to_center = (get_parent().global_position - global_position)
+    var forward = to_center.rotated(PI/2).normalized()
+    var projected = movement.dot(forward)
+    #torque = projected/500#/500*(500-position.length())/150.0
+    torque = -Vector3(to_center.x, 0, to_center.y).cross(Vector3(movement.x, 0, movement.y)).y/300000
+    #print(torque)
+
+    if Vector2(right,down) != Vector2.ZERO and speed > 10:
+        $Sprite.rotation = 0.1*sin(OS.get_system_time_msecs()/50)
